ed6b251014ac791f05017dda478bbd3287fe708d
tolumide-ng <tolumideshopein@gmail.com>
2019-06-16 11:18:08 +0100

    catchAll Route
    - Import Switch from react-router-dom to employ only the first match

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import './App.css'
 
 const Home = () => <h1>Home</h1>
@@ -18,6 +18,7 @@ const Links = () => (
     <NavLink to='/12-12-2013.rr'>RegularExpressions</NavLink>
     <NavLink to='/bantu/?id=123'>Bantu</NavLink>
     <NavLink to={{pathname: '/bantu', search: 'id=456'}}>BBantu</NavLink>
+    <NavLink to='/aPageThatDoesNotExist'>Exist?</NavLink>
   </nav>
 )
 
@@ -25,22 +26,24 @@ const App = () => (
   <Router>
     <div>
       <Links />
-      <Route exact path='/' component={Home} />
-      <Route path='/about' render={() => <h1>About Page</h1>} />
-      <Route path='/contact' render={() => <h1>Contact Page</h1>} />
-      {/* Adding the question mark to the end of the parameter on the route path makes it optional for the user */}
-      <Route path='/page/:number?' render={({match}) => (<h2>Display Number: {match.params.number}</h2>)} />
-      {/* Using regular expressions to validate url matches */}
-      <Route path='/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)' render={({match}) => (
-        <h2>Date: {match.params.a}<br />form: {match.params.b || 'bolanle'}</h2>)} 
-      />
-      <Route path='/bantu' render={({match, location}) => (
-        <div>
-          <h1>{JSON.stringify(match)}</h1> 
-          <h1>{JSON.stringify(location)}</h1>
-          <h1>{new URLSearchParams(location.search).get('id')}</h1>
-      </div>
-      )} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' render={() => <h1>About Page</h1>} />
+        <Route path='/contact' render={() => <h1>Contact Page</h1>} />
+        {/* Adding the question mark to the end of the parameter on the route path makes it optional for the user */}
+        <Route path='/page/:number?' render={({match}) => (<h2>Display Number: {match.params.number}</h2>)} />
+        {/* Using regular expressions to validate url matches */}
+        <Route path='/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)' render={({match}) => (
+          <h2>Date: {match.params.a}<br />form: {match.params.b || 'bolanle'}</h2>)} 
+        />
+        <Route path='/bantu' render={({match, location}) => (
+          <div>
+            <h1>{JSON.stringify(match)}</h1> 
+            <h1>{JSON.stringify(location)}</h1>
+            <h1>{new URLSearchParams(location.search).get('id')}</h1>
+        </div>
+        )} />
+      </Switch>
     </div>
   </Router>
 )
