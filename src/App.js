22cbf7cc37b6d4b1b90be0c618ef864fdf9a6266
tolumide-ng <tolumideshopein@gmail.com>
2019-06-16 08:35:12 +0100

    Link for easy navigation across Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Home = () => <h1>Home</h1>
+
+const Links = () => (
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to={{pathname: '/about'}}>About</Link>
+    <Link replace to='/contact'>Contact</Link>
+  </nav>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <Links />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' render={() => <h1>About Page</h1>} />
+      <Route path='/contact' render={() => <h1>Conact Page</h1>} />
     </div>
-  );
-}
+  </Router>
+)
 
-export default App;
+export default App
