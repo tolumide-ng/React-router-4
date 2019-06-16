e6be5461fe0355f8ed0753c48a53c7385de3ea70
tolumide-ng <tolumideshopein@gmail.com>
2019-06-16 08:53:45 +0100

    isActive Property of NavLink

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css'
 
 const Home = () => <h1>Home</h1>
 
+const isActiveFunc = (match, location) => {
+  console.log(match, location);
+  return false;
+}
+
 const Links = () => (
   <nav>
-    <NavLink exact to='/'>Home</NavLink>
+    <NavLink isActive={isActiveFunc} exact to='/'>Home</NavLink>
     <NavLink to={{pathname: '/about'}}>About</NavLink>
     <NavLink replace to='/contact'>Contact</NavLink>
   </nav>
@@ -18,7 +23,7 @@ const App = () => (
       <Links />
       <Route exact path='/' component={Home} />
       <Route path='/about' render={() => <h1>About Page</h1>} />
-      <Route path='/contact' render={() => <h1>Conact Page</h1>} />
+      <Route path='/contact' render={() => <h1>Contact Page</h1>} />
     </div>
   </Router>
 )
